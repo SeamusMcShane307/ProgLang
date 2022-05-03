4dfc7b6165692d5bc9b309f6f1351c85fac4385c
SeamusMcShane307 <56978513+SeamusMcShane307@users.noreply.github.com>
2022-05-03 11:55:36 -0500

    Update main.c

--- main.c
+++ main.c
@@ -3,26 +3,49 @@
 #include <ctype.h>
 #include <string.h>
 
-char *input = malloc(sizeof(char*)), *allIn = input ;
-int numStr=0;
+char *input = NULL, *allIn = NULL ;
 size_t lineLen = 3;
 
 int main() {
-    printf("Enter function\n");
+    printf("Enter your commands\n");
     /*scanf("%s", input = realloc(input, 3 * sizeof(char)));
     printf("%c", *(input+3));
     printf("%d", iscntrl(*(input+3)));
      */
     getline(&input, &lineLen, stdin);
-    numStr++;
+    printf("%i\n", sizeof(input) );
+    allIn = malloc(sizeof(input));
+    for( int i = 0; i < strlen(input); i++){
+        printf("%i\n", i );
+        *(allIn + i) = *(input + i);
+    }
+
     while(strcmp( input, "$$\n") != 0) {
         printf("%s", input);
         printf("%i\n", strlen(input));
-        realloc(input, lineLen);
-        getline(&input, &lineLen, stdin);
-        allIn = allIn + *input;
-        numStr++;
+        printf("%s", "input memory size");
+        printf("%i\n", sizeof(input) );
         printf("%s", allIn);
+        printf("%i\n", strlen(allIn));
+
+        getline(&input, &lineLen, stdin);
+        if(strcmp( input, "$$\n") != 0){
+            realloc(allIn, sizeof(allIn) + sizeof(input));
+            strncat(allIn, input, strlen(input));
+        }
     }
+    free(input);
+    printf("%s", allIn);
+    printf("%i\n", strlen(allIn));
+
+    /*
+    char *token = NULL;
+    int currToken = 0;
+    while(strcmp(token, "$$") != 0){
+        switch (*(all)) {
+
+        }
+    }
+     */
     return 0;
 }
